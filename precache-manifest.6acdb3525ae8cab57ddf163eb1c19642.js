df831653ae71eb248b832d7adac974cc7e1b674c
Mohammad Hamzah Iqbal <mohammad.iqbal.7@city.ac.uk>
2022-07-06 01:40:17 +0100

    Updates

--- precache-manifest.6acdb3525ae8cab57ddf163eb1c19642.js
+++ precache-manifest.6acdb3525ae8cab57ddf163eb1c19642.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8c151e4697456512fd99",
+    "revision": "0d4dd88867c3c1b8de86",
     "url": "/crypto-position-calculator/static/css/main.3d92d5e5.chunk.css"
   },
   {
-    "revision": "8c151e4697456512fd99",
-    "url": "/crypto-position-calculator/static/js/main.8c151e46.chunk.js"
+    "revision": "0d4dd88867c3c1b8de86",
+    "url": "/crypto-position-calculator/static/js/main.0d4dd888.chunk.js"
   },
   {
     "revision": "6d1c687f73f1a0446a07",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/crypto-position-calculator/static/js/runtime~main.8c0fa573.js"
   },
   {
-    "revision": "cd3b3f1a2b28b179f3b7c36ea080d0d0",
+    "revision": "f9248b022549358904deb296450df957",
     "url": "/crypto-position-calculator/index.html"
   }
 ];
