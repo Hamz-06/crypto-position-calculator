64d53128f65a92e04589a7988ff09b438295007e
Mohammad Hamzah Iqbal <mohammad.iqbal.7@city.ac.uk>
2022-07-29 01:05:51 +0100

    version 4 .1 - added env

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -9,14 +9,15 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDp8Yn2z7WsPJVaICzzwKLctyw1rb1oi3k",
-  authDomain: "crytotracker-2a85b.firebaseapp.com",
-  projectId: "crytotracker-2a85b",
-  storageBucket: "crytotracker-2a85b.appspot.com",
-  messagingSenderId: "861000309447",
-  appId: "1:861000309447:web:c97953566139440fcbfce2",
-  measurementId: "G-M228LWPC2M"
+  apiKey:`${process.env.REACT_APP_APIKEY}`,
+  authDomain:`${process.env.REACT_APP_APIAUTHDOM}`,
+  projectId: `${process.env.REACT_APP_PROJID}`,
+  storageBucket: `${process.env.REACT_APP_STORBUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGESENDER}`,
+  appId: `${process.env.REACT_APP_APPID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENTID}`
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
